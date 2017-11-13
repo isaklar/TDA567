011a6462330295c11fbe73de236d12e6c413b7a1
Isak Einler Larsson <isakeinlerlarsson@isaks-mbp.mynet>
2017-11-13 16:32:31 +0100

    almost done, 1-2 more needs

--- Laboration_1/Lab1BlackBox.java
+++ Laboration_1/Lab1BlackBox.java
@@ -1,11 +1,14 @@
 import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import java.util.Arrays;
 
 public class Lab1BlackBox{
   private WorkSchedule schedule;
 
   // starttime < 0
   @Test
-  public void startTimeZero(){
+  public void testStartTimeZero(){
     int size = 10; // random size
     schedule = new WorkSchedule(size);
     schedule.setRequiredNumber(1, 0, 9);
@@ -15,7 +18,7 @@ public class Lab1BlackBox{
 
   // endtime >= size
   @Test
-  public void endTimeSize(){
+  public void testEndTimeSize(){
     int size = 10; // random size
     schedule = new WorkSchedule(size);
     schedule.setRequiredNumber(1, 0, 9);
@@ -23,4 +26,35 @@ public class Lab1BlackBox{
     assertFalse(result);
   }
 
+  // starttime > endtime
+  @Test
+  public void testEndTimeStartTime(){
+    int size = 10; // random size
+    schedule = new WorkSchedule(size);
+    schedule.setRequiredNumber(1, 0, 9);
+    boolean result = schedule.addWorkingPeriod("employee", size+1, size);
+    assertFalse(result);
+  }
+
+  //  add workingEmployee > requiredNumber
+  @Test
+  public void testRequiredNumber(){
+    int size = 10; // random size
+    schedule = new WorkSchedule(size);
+    schedule.setRequiredNumber(1, 0, 9);
+    schedule.addWorkingPeriod("employee1", size, size);
+    boolean result = schedule.addWorkingPeriod("employee2", size, size);
+    assertFalse(result);
+  }
+
+  // any workingEmployee == employee
+  @Test
+  public void testEmployeeExist(){
+    int size = 10; // random
+    schedule = new WorkSchedule(size);
+    schedule.setRequiredNumber(2, 0, 9);
+    schedule. addWorkingPeriod("employee", size, size);
+    boolean result = schedule.addWorkingPeriod("employee", size, size);
+    assertFalse(result);
+  }
 }
