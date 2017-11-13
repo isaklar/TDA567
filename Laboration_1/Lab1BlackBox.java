0a4aec27b8d53e5811a1bb17f3f3e66ff9f5433f
Isak Einler Larsson <isakeinlerlarsson@Isaks-MacBook-Pro.local>
2017-11-13 14:48:40 +0100

    added 2 partitions

--- Laboration_1/Lab1BlackBox.java
+++ Laboration_1/Lab1BlackBox.java
@@ -1,5 +1,26 @@
 import org.junit.Test;
 
 public class Lab1BlackBox{
-  
+  private WorkSchedule schedule;
+
+  // starttime < 0
+  @Test
+  public void startTimeZero(){
+    int size = 10; // random size
+    schedule = new WorkSchedule(size);
+    schedule.setRequiredNumber(1, 0, 9);
+    boolean result = schedule.addWorkingPeriod("employee", size-1, size);
+    assertFalse(result);
+  }
+
+  // endtime >= size
+  @Test
+  public void endTimeSize(){
+    int size = 10; // random size
+    schedule = new WorkSchedule(size);
+    schedule.setRequiredNumber(1, 0, 9);
+    boolean result = schedule.addWorkingPeriod("employee", size, size+1);
+    assertFalse(result);
+  }
+
 }
