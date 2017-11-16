328bb87967fae9e97a7c16a4aed78fd11f24d0a9
Isak Einler Larsson <isakeinlerlarsson@isaks-mbp.mynet>
2017-11-16 15:12:58 +0100

    blackbox borde vara klar nu

--- Laboration_1/Lab1BlackBox.java
+++ Laboration_1/Lab1BlackBox.java
@@ -58,7 +58,7 @@ public class Lab1BlackBox{
     int size = 10; // random
     schedule = new WorkSchedule(size);
     schedule.setRequiredNumber(2, 0, 9);
-    schedule. addWorkingPeriod("employee", size, size);
+    schedule.addWorkingPeriod("employee", size, size);
     boolean result = schedule.addWorkingPeriod("employee", size, size);
     assertFalse(result);
   }
@@ -68,6 +68,28 @@ public class Lab1BlackBox{
   @Test
   public void testWhenSuccessA(){
     int size = 10;
+		schedule = new WorkSchedule(size);
+		WorkSchedule dummySchedule = new WorkSchedule(size);
+		schedule.setRequiredNumber(2, 2, 7); // random hours
+		schedule.addWorkingPeriod("employee1", 5, 5);
+		dummySchedule.setRequiredNumber(2, 2, 7);
+		dummySchedule.addWorkingPeriod("employee1", 5, 5);
+    boolean result = schedule.addWorkingPeriod("employee2", 2, 7);
+    assertTrue(result);
+
+    for(int i = 0; i < size; i++) {
+			WorkSchedule.Hour h1 = schedule.readSchedule(i);
+			WorkSchedule.Hour h2 = dummySchedule.readSchedule(i);
+
+			if(i >= 2 && i <= 7) {
+				// workingEmployees contain a string equal to employee
+				assertTrue(Arrays.asList(h1.workingEmployees).contains("employee2"));
+			} else {
+				// Checks if schedule is unchanged with dummySchedule
+				assertTrue(h1.requiredNumber == h2.requiredNumber);
+				assertTrue(Arrays.deepEquals(h1.workingEmployees, h2.workingEmployees));
+			}
+		}
   }
 
   /* ####################################################################
@@ -88,9 +110,15 @@ public class Lab1BlackBox{
 	}
 
   // when test goes through
-  // TODO
   @Test
   public void testWhenSuccessB(){
     int size = 10;
+    schedule = new WorkSchedule(size);
+    schedule.setRequiredNumber(1, 0, 9);
+    schedule.addWorkingPeriod("employee", 0, 9);
+    String[] employees = schedule.workingEmployees(0,9);
+    // returns an array with distinct strings -- a string appears in the return array if and only if
+    // it appears in the workingEmployees of at least one hour in the interval starttime to endtime
+    assertTrue(Arrays.asList(employees).contains("employee"));
   }
 }
